fbf6b1d8caa63ff550e2628d66876767ff45e9d5
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-04-25 13:59:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,10 @@
 def starts_with_a_vowel?(word)
-"The rain in Spain lies mainly in the plain"
+word = ["The rain in Spain lies mainly in the plain"]
+word.scan(/^\W\b([aAeEiIoOuU]))
+return true
+else
+false
+end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
